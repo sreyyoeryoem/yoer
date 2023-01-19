98432b5476c7ac6d86ccad186d08c81039f0074d
YOEM.SREYYOER <YOEM.SREYYOER@PNC.LAN>
2023-01-19 13:43:25 +0700

    [PO-5] create para

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
      <h1>Hello</h1>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia pariatur, velit quibusdam excepturi accusantium vel officiis commodi cupiditate accusamus, minima reiciendis quia omnis corrupti assumenda natus, rem temporibus adipisci beatae.</p>
 </body>
 </html>
